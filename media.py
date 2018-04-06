eab43bd89378ed31d891d5f8dd4b114e74ca1704
Don Coffman <coffmand@live.com>
2018-04-06 08:51:54 -0500

    PEP8 cleanup

--- media.py
+++ media.py
@@ -1,21 +1,21 @@
-#=================================================================
+# =================================================================
 # Python program: media.py
-# - Classes for movie_website project 
-#=================================================================
+# - Classes for movie_website project
+# =================================================================
 
 import webbrowser
+
 
 class Movie():
     """This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title               = movie_title
-        self.storyline           = movie_storyline
-        self.poster_image_url    = poster_image
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-
